12565d5ebfde7b6731ccef929d780613d5d45783
Luiz Carlos Szpikula Junior <luiz.szpikula@alfaconcursos.com.br>
2016-11-14 23:27:58 -0200

    Parte 4: Desenvolvendo a autenticação do sistema. Criado model, controller, repository, entity, filter para usuario. Criado tela de login em index.xhtml

---
+++ Appjsf/src/main/java/br/univel/luizszpikula/repository/UsuarioRepository.java
@@ -0,0 +1,39 @@
+package br.univel.luizszpikula.repository;
+
+import java.io.Serializable;
+
+import javax.persistence.Query;
+
+import br.univel.luizszpikula.model.UsuarioModel;
+import br.univel.luizszpikula.repository.entity.UsuarioEntity;
+import br.univel.luizszpikula.uteis.Uteis;
+
+
+public class UsuarioRepository implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+
+	public UsuarioEntity ValidaUsuario(UsuarioModel usuarioModel){
+
+		try {
+
+			//QUERY QUE VAI SER EXECUTADA (UsuarioEntity.findUser)
+			Query query = Uteis.JpaEntityManager().createNamedQuery("UsuarioEntity.findUser");
+
+			//PARÂMETROS DA QUERY
+			query.setParameter("usuario", usuarioModel.getUsuario());
+			query.setParameter("senha", usuarioModel.getSenha());
+
+			//RETORNA O USUÁRIO SE FOR LOCALIZADO
+			return (UsuarioEntity)query.getSingleResult();
+
+		} catch (Exception e) {
+
+			return null;
+		}
+
+
+
+	}
+}
